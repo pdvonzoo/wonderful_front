ee026677bf6eb85a436dc559b29f0f872e5b8c4b
KONAcar <wlatmdqja@naver.com>
2020-01-07 00:43:45 +0900

    인피니티 스크롤링 에러 수정 완료

--- src/components/Book/searchResultList.js
+++ src/components/Book/searchResultList.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { SEARCH_BOOK_REQUEST } from '../../modules/books'
-const searchResultList = () => {
-    const { searchResultBooks, isLoadging } = useSelector(state => state.books);
-    const dispatch = useDispatch();
+import SearchABook from './searchABook';
 
-    const getBooks = () => {
-        dispatch({ type: SEARCH_BOOK_REQUEST, offset: searchResultBooks.length })
+const searchResultList = () => {
+
+    const { searchResultBooks, isLoadging, hasMoreSearchBooks } = useSelector(state => state.books);
+    const dispatch = useDispatch();
+    const onScroll = () => {
+        // console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight)
+        if (window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 350) {
+            if (hasMoreSearchBooks) {
+                dispatch({ type: SEARCH_BOOK_REQUEST, offset: searchResultBooks.length })
+            }
+
+        }
     }
+    useEffect(() => {
+        window.addEventListener('scroll', onScroll);
+        return () => {
+            window.removeEventListener('scroll', onScroll)
+        }
+    }, [searchResultBooks.length])
+
 
     return (
         <>
-            {isLoadging && <h1>로딩 중......</h1>}
-            {!isLoadging && searchResultBooks.map((book, index) => {
+            {/* {isLoadging && <h1>로딩 중......</h1>} */}
+            {searchResultBooks.map((book, index) => {
                 return (
-                    <div key={index}>
-                        <hr />
-                        <li>  <img src={book.image} />
-                            /        책 이름 : {book.title} / 책 저자 : {book.author} / 출판 날짜 :{book.pubdate}  / isbn:{book.isbn}
-                            <button>대여하기</button>
-                        </li>
-                    </div>
+                    <SearchABook key={index} title={book.title} author={book.author} image={book.image} pubdate={book.pubdate} isbn={book.isbn} />
                 )
             })}
-            <button onClick={getBooks}>더 가져오기</button>
         </>
     );
 };
