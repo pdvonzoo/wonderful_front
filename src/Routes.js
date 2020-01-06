d69a8983a5703bac33016bc5880cb0c8a0176eaa
pdvonzoo <a01088570194@gmail.com>
2020-01-06 13:32:28 +0900

    refactoring: sign in 컴포넌트 제거 및 sign up 이름 auth로 변경

--- src/Routes.js
+++ src/Routes.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
 import Layout from './components/common/Layout'
-import { Admin, BookList, Home, Error, Signin, Signup, UserHistory, searchPage } from "./pages";
+import { Admin, BookList, Home, Error, Auth, UserHistory, searchPage } from "./pages";
 
 const Routes = () => {
 
@@ -12,12 +11,14 @@ const Routes = () => {
       <Layout />
       <Switch>
         <Route path="/" exact component={Home} />
-        <Route path="/signin" exact component={Signin} />
-        <Route path="/signup" exact component={Signup} />
+        <Route path="/auth" exact component={Auth} />
         <Route path="/error" exact component={Error} />
         <Route path="/userhistory" exact component={UserHistory} />
+        <Route path="/admin" exact component={Admin} />
+
         <Route path="/search" component={searchPage} />
-        <Route path="/admin" component={Admin} />
+        <Route path="*" component={Home} />
+
       </Switch>
     </Router>
   );
